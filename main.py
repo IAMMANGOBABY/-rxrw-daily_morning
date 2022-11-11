5a9031154e116b39ef0a1018fc04e1d82e6fca52
IAMMANGOBABY <59330116+IAMMANGOBABY@users.noreply.github.com>
2022-11-11 17:58:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import random
 today = datetime.now()
 start_date = os.environ['2020-12-13']
 city = os.environ['北京']
-birthday = os.environ['1997-12-17']
+birthday = os.environ['12-17']
 
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
